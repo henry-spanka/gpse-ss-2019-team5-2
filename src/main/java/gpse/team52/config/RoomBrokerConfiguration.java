bd1f1581e9a6ea3ed80184c761352503c937a413
Henry Spanka <hspanka@techfak.uni-bielefeld.de>
2019-04-28 22:46:25 +0200

    fix circular dependency of password encoder

--- src/main/java/gpse/team52/config/RoomBrokerConfiguration.java
+++ src/main/java/gpse/team52/config/RoomBrokerConfiguration.java
@@ -1,28 +1,17 @@
 package gpse.team52.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
- * Registers the AuthenticationService and PasswordEncoder to use.
+ * Basic RoomBroker configuration.
  */
 @Configuration
 @ComponentScan
 public class RoomBrokerConfiguration {
-    @Autowired
-    public void configureGlobal(final UserDetailsService userDetailsService,
-                                final PasswordEncoder passwordEncoder,
-                                final AuthenticationManagerBuilder auth) throws Exception { //NOPMD
-        auth.userDetailsService(userDetailsService)
-        .passwordEncoder(passwordEncoder);
-    }
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
--- src/main/java/gpse/team52/config/WebSecurityConfig.java
+++ src/main/java/gpse/team52/config/WebSecurityConfig.java
@@ -1,9 +1,13 @@
 package gpse.team52.config;
 
+import gpse.team52.contract.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Spring Security Configuration.
@@ -32,5 +36,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .and()
         .logout()
         .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(final UserService userDetailsService,
+                                final PasswordEncoder passwordEncoder,
+                                final AuthenticationManagerBuilder auth) throws Exception { //NOPMD
+        auth.userDetailsService(userDetailsService)
+        .passwordEncoder(passwordEncoder);
     }
 }
