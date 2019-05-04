d9470433a10797ea4dcb7f5a704ca41472382b4f
dohagemeier <dohagemeier@techfak.uni-bielefeld.de>
2019-05-04 14:03:44 +0200

    layout changes, added file explorer for choosing a profile pic refs #61883 closes #61882

--- src/main/java/gpse/team52/fileManagerController.java
+++ src/main/java/gpse/team52/fileManagerController.java
@@ -1,18 +1,14 @@
 package gpse.team52;
 
-import gpse.team52.domain.User;
-import gpse.team52.repository.UserRepository;
-import gpse.team52.service.UserServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class ProfileController {
-    @GetMapping("/profile")
+public class fileManagerController {
+    @GetMapping("/fileManager")
     public ModelAndView dashboard() {
-        final ModelAndView modelAndView = new ModelAndView("profile");
-
+        final ModelAndView modelAndView = new ModelAndView("fileManager");
         return modelAndView;
     }
 
