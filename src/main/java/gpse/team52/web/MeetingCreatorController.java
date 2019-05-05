ba41c5c71dbc636c35d1f58f0568ae6972c45bc3
sjaeger <sjaeger@techfak.uni-bielefeld.de.de>
2019-05-05 17:35:52 +0200

    refs #62113 : automatic completion of the date, when checkbox is checked

--- src/main/java/gpse/team52/web/MeetingCreatorController.java
+++ src/main/java/gpse/team52/web/MeetingCreatorController.java
@@ -2,10 +2,19 @@ package gpse.team52.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MeetingCreatorController {
+
+
+    @PostMapping("/createMeeting")
+    public void checkBoxAction (@RequestParam String checkbox){
+
+    }
+
     @GetMapping("/createMeeting")
     public ModelAndView createMeeting() {
         final ModelAndView modelAndView = new ModelAndView("createMeeting");
