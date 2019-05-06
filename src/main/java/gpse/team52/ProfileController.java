9988d42f7935cd23603304dc9eedcb704554c03a
dohagemeier <dohagemeier@techfak.uni-bielefeld.de>
2019-05-06 11:09:12 +0200

    profile now shows some actuall user data refs #61889

--- src/main/java/gpse/team52/ProfileController.java
+++ src/main/java/gpse/team52/ProfileController.java
@@ -3,6 +3,8 @@ package gpse.team52;
 import gpse.team52.domain.User;
 import gpse.team52.repository.UserRepository;
 import gpse.team52.service.UserServiceImpl;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,8 +12,11 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class ProfileController {
     @GetMapping("/profile")
-    public ModelAndView dashboard() {
+    public ModelAndView profile(Authentication authentication) {
         final ModelAndView modelAndView = new ModelAndView("profile");
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        modelAndView.addObject("username", userDetails.getUsername());
+        modelAndView.addObject("e-mail", userDetails.)
 
         return modelAndView;
     }
--- src/main/java/gpse/team52/editProfileController.java
+++ src/main/java/gpse/team52/editProfileController.java
@@ -1,5 +1,7 @@
 package gpse.team52;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,7 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class editProfileController {
     @GetMapping("/editProfile")
-    public ModelAndView dashboard() {
+    public ModelAndView editProfile() {
         final ModelAndView modelAndView = new ModelAndView("editProfile");
         return modelAndView;
     }
