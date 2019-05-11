206a8ba2c9157c5b07c290407a33807da49e34ed
Melina Müller <memueller@techfak.uni-bielefeld.de>
2019-05-11 12:21:52 +0200

    frontend dataimport refs #62300

---
+++ src/main/java/gpse/team52/web/DatamanagementController.java
@@ -0,0 +1,36 @@
+package gpse.team52.web;
+
+import java.util.UUID;
+
+import javax.validation.Valid;
+
+import gpse.team52.contract.UserService;
+import gpse.team52.domain.User;
+import gpse.team52.exception.EmailExistsException;
+import gpse.team52.exception.InvalidConfirmationTokenException;
+import gpse.team52.exception.UsernameExistsException;
+import gpse.team52.form.UserRegistrationForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DatamanagementController {
+
+    /**
+     * Show the datamanagement form to the user.
+     *
+     * @return Datamanagement view.
+     */
+    @GetMapping("/datamanagement")
+    public ModelAndView showDatamanagement() {
+        final ModelAndView modelAndView = new ModelAndView("datamanagement");
+        return modelAndView;
+    }
+}
