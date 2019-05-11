45f24e1d8530e0a76451d4361c6dd4a1abdfa5f8
Julius ELlermann <jellermann@techfak.uni-bielefeld.de>
2019-05-11 17:01:16 +0200

    Changed appeal of chronik.html and meeting.html to fit the startpage. refs #62368

--- src/main/java/gpse/team52/seeder/InitializeDefaultMeetings.java
+++ src/main/java/gpse/team52/seeder/InitializeDefaultMeetings.java
@@ -77,7 +77,7 @@ public class InitializeDefaultMeetings {
         Location location1 = roomService.createLocation("Bielefeld");
         Location location2 = roomService.createLocation("Gütersloh");
         Room room1 = roomService.createRoom(12, 2, "bielefeldroom@example.de", location1);
-        Room room2 = roomService.createRoom(8, 0, "guetersloh@example.de", location2);
+        Room room2 = roomService.createRoom(8, 0, "gueterslohroom@example.de", location2);
 
         final Meeting meeting1 = new Meeting("Tolles Meeting", 23, room1);
         meeting1.setStartAt(LocalDateTime.of(2019, 5, 10, 10, 15));
--- src/main/java/gpse/team52/web/MeetingController.java
+++ src/main/java/gpse/team52/web/MeetingController.java
@@ -1,4 +1,44 @@
 package gpse.team52.web;
 
+import gpse.team52.contract.MeetingService;
+import gpse.team52.contract.UserService;
+import gpse.team52.domain.Meeting;
+import gpse.team52.domain.Participant;
+import gpse.team52.domain.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
 public class MeetingController {
+    @Autowired
+    private MeetingService meetingService;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/meeting/{id}")
+    public ModelAndView meeting(@PathVariable("id") final String id, Authentication authentication) {
+        final ModelAndView modelAndView = new ModelAndView("meeting");
+
+        Meeting meeting = meetingService.getMeetingById(id);
+        modelAndView.addObject("meeting", meeting);
+
+        User user = (User) authentication.getPrincipal();
+        if (user.getUsername() == meeting.getOwner().getUsername()) {
+            System.out.println(user.getUsername());
+            /*
+        List<Participant> participants = meeting.getParticipants();
+        System.out.println(participants.get(0).getFirstName());
+        modelAndView.addObject("participants", participants);
+         */
+        }
+
+        return modelAndView;
+    }
 }
