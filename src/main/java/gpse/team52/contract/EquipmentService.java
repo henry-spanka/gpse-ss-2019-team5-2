e75f3e4cae9a29a4393eb71bdc80a1cb59d9dcf0
Julius ELlermann <jellermann@techfak.uni-bielefeld.de>
2019-05-15 15:49:53 +0200

    Testdata for Equipment is now available and functional. Equipment is shown on meeting.html. refs #62368

--- src/main/java/gpse/team52/contract/EquipmentService.java
+++ src/main/java/gpse/team52/contract/EquipmentService.java
@@ -2,9 +2,8 @@ package gpse.team52.contract;
 
 import gpse.team52.domain.Equipment;
 
-
 public interface EquipmentService {
-    Equipment createEquipment(String equipmentName, String consumable, int getRoomID, boolean defect);
+    Equipment createEquipment(String equipmentName);
 
     Iterable<Equipment> getAllEquipments();
 }
