21d880bd56d3ebb7deed4ecef5fa4127d4fb15ae
sjaeger <sjaeger@techfak.uni-bielefeld.de.de>
2019-06-14 18:41:44 +0200

    refs #62620 : uploaded file is still null, don't know why

--- src/main/java/gpse/team52/web/DatamanagementController.java
+++ src/main/java/gpse/team52/web/DatamanagementController.java
@@ -11,16 +11,16 @@ import gpse.team52.exception.InvalidConfirmationTokenException;
 import gpse.team52.exception.UsernameExistsException;
 import gpse.team52.form.UserRegistrationForm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.config.RepositoryBeanDefinitionRegistrarSupport;
 import org.springframework.mail.MailException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@Controller
+@RestController 
 public class DatamanagementController {
 
     /**
@@ -33,4 +33,26 @@ public class DatamanagementController {
         final ModelAndView modelAndView = new ModelAndView("datamanagement");
         return modelAndView;
     }
+
+    @PostMapping("/datamanagement")
+    public ModelAndView submitData( @RequestParam(value = "file", required = false) MultipartFile file, RedirectAttributes redirectAttributes) {
+        System.out.println("flubb");
+        //check if there is an uploaded file
+       if (file != null && !file.isEmpty()) {
+           // dbFileStorageService.store(file);
+           // user.setPicture(file.getOriginalFilename());
+           System.out.println("hallo");
+        }if (file == null){
+           System.out.println("dong");
+        }/*if(file.isEmpty()){
+           System.out.println("witch is dead");
+        }*/
+
+       /* user.setFirstname(createUserCmd.getFirstname());
+        user.setLastname(createUserCmd.getLastname());
+        user.setLocation(createUserCmd.getLocation());
+        userService.updateUser(user);*/
+        return new ModelAndView("redirect:/start");
+    }
 }
+
