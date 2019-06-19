21145204dea6d6feb0b0fb0cc6a541cbe8ae3a37
Lukas Dyballa <ldyballa@techfak.uni-bielefeld.de>
2019-06-19 13:41:51 +0200

    refs #63001 small changes to html site

--- src/main/java/gpse/team52/web/EditMeetingController.java
+++ src/main/java/gpse/team52/web/EditMeetingController.java
@@ -1,21 +1,37 @@
 package gpse.team52.web;
 
-import gpse.team52.contract.MeetingService;
+import gpse.team52.contract.*;
 import gpse.team52.domain.Meeting;
+import gpse.team52.domain.Participant;
+import gpse.team52.form.MeetingCreationForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Edit meeting controller.
  */
 @Controller
+@SessionAttributes("meeting")
 public class EditMeetingController {
 
     @Autowired
+    private UserService userService;
+
+    @Autowired
+    private LocationService locationService;
+
+    @Autowired
     private MeetingService meetingService;
+
+    @Autowired
+    private RoomService roomService;
+
+    @Autowired
+    private RoomFinderService roomFinderService;
+
+
 
     /**
      * returns page to edit meetings.
@@ -27,6 +43,9 @@ public class EditMeetingController {
         final Meeting meeting = meetingService.getMeetingById(id);
         ModelAndView modelAndView = new ModelAndView("editMeeting");
         modelAndView.addObject("meeting", meeting);
+        modelAndView.addObject("users", userService.getAllUsers());
+        modelAndView.addObject("locations", locationService.getAllLocations());
+        modelAndView.addObject("equipments", roomService.getAllEquipment());
 
         return modelAndView;
     }
