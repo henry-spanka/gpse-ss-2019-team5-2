7e041d5470c4e1363f39de812a3be7f878398a92
sjaeger <sjaeger@techfak.uni-bielefeld.de.de>
2019-06-29 12:10:06 +0200

    refs #61435 : Change of data proceeding method and adaption of the frontend

--- src/main/java/gpse/team52/domain/DataImport.java
+++ src/main/java/gpse/team52/domain/DataImport.java
@@ -1,21 +1,26 @@
 package gpse.team52.domain;
 
 
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
 import gpse.team52.contract.mail.MailService;
 import gpse.team52.service.mail.MailServiceImpl;
 import org.springframework.mail.MailException;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
-import java.io.BufferedReader;
-import java.io.FileReader;
+
+import java.io.*;
 
 public class DataImport {
 
-   // private final MailServiceImpl mailService = new MailServiceImpl() ;
+    // private final MailServiceImpl mailService = new MailServiceImpl() ;
 
     /*This method handles the import of user files
-    * when a csv file containing firstname, lastname and mail adress
-    * is uploded the person gets an email*/
-     public void csvUserImport(String path) {
+     * when a csv file containing firstname, lastname and mail adress
+     * is uploded the person gets an email*/
+    public void csvUserImport(String path) {
         try {
             FileReader reader = new FileReader(path);
             BufferedReader inBuffer = new BufferedReader(reader);
@@ -23,11 +28,11 @@ public class DataImport {
 
 
             while ((line = inBuffer.readLine()) != null) {
-                String[] split =line.split(";");
-                if(split.length!=3){
+                String[] split = line.split(";");
+                if (split.length != 3) {
                     throw new Exception();
 
-                    }
+                }
                 final ModelAndView modelAndView = new ModelAndView("email/mail-import.html");
 
 
@@ -40,81 +45,85 @@ public class DataImport {
                     public void sendEmailTemplateToUser(User user, String subject, ModelAndView template) throws MailException {
 
                     }*/
-                }
-
-               // mailService.sendEmailTemplateToUser(user, "Email Verification", modelAndView);
-
-
-
-
-        } catch (Exception e) {
-            System.out.println("Required format: csv containing: firstname, lastname, mail without header; seperated by ; ");
-        }
-    }
-
-    /*This method handles the import of room files*/
-
-    static public void csvRoomImport(String path) {
-        try {
-            int counter = 0;
-            FileReader reader = new FileReader(path);
-            BufferedReader inBuffer = new BufferedReader(reader);
-            String line = null;
-            Boolean isFirstLineHeader = true;
-                        while ((line = inBuffer.readLine()) != null) {
-                if(isFirstLineHeader){
-                    isFirstLineHeader=false;
-
-                    continue;
-                }
-                String[] subSet = line.split(";");
-
-                if (subSet.length != 8) {
-                    System.out.println(subSet.length);
-
-                    throw new Exception();
-                } else {
-
-                    Room room = new Room();
-                    Location location = new Location(subSet[0]);
-                    room.setLocation(location);
-                    room.setRoomName(subSet[1]);
-
-                    //number persons split for extra seats
-                    String[] seats = subSet[2].split("\\+");
-                    if (seats.length == 1) {
-                        room.setSeats(Integer.parseInt(seats[0]));
-                    } else if (seats.length == 2) {
-                        room.setSeats(Integer.parseInt(seats[0]));
-                        room.setExpandableSeats(Integer.parseInt(seats[1]));
-                    }
-                    //3 spliting for different equiptment items
-
-                    String[] equiptments = subSet[3].split(",");
-                    for (int i = 0; i < equiptments.length; i++) {
-                        Equipment equipment = new Equipment();
-                        room.addEquipment (equipment);
-                       equipment.addRoom(room);}
-
-
-                        room.setTelephone(subSet[4]);
-
-
-                    room.setNotes(subSet[5]);
-
-                    room.setOffice(subSet[6]);
-
-                    room.setRoomEmail(subSet[7]);
-
-                }
-
-
             }
 
+            // mailService.sendEmailTemplateToUser(user, "Email Verification", modelAndView);
+
 
         } catch (Exception e) {
-            System.out.println(" Exception: file is not a csv file or the layout does not fit.");
+            System.out.println("Required format: csv containing: firstname, lastname, mail with header; seperated by ; ");
         }
     }
+
+    /*This method handles the import of csv files
+     * containing room or User data. It is implied that there is a header*/
+
+    static public void csvImport(final MultipartFile file) {
+        try {
+            Reader reader = new InputStreamReader(file.getInputStream());
+            CSVReader csvReader = new CSVReaderBuilder(reader).withCSVParser(new CSVParserBuilder().withSeparator(';').build()).withSkipLines(1).build();
+            String line[] = null;
+            while ((line = csvReader.readNext())!= null){
+
+
+            if (line.length==8) {
+
+                Room room = new Room();
+                Location location = new Location(line[0]);
+                room.setLocation(location);
+                room.setRoomName(line[1]);
+
+                //number persons split for extra seats
+                String[] seats = line[2].split("\\+");
+                if (seats.length == 1) {
+                    room.setSeats(Integer.parseInt(seats[0]));
+                } else if (seats.length == 2) {
+                    room.setSeats(Integer.parseInt(seats[0]));
+                    room.setExpandableSeats(Integer.parseInt(seats[1]));
+                }
+                //3 spliting for different equiptment items
+
+                String[] equiptments = line[3].split(",");
+                for (int i = 0; i < equiptments.length; i++) {
+                    Equipment equipment = new Equipment();
+                    room.addEquipment(equipment);
+                    equipment.addRoom(room);
+                }
+
+
+                room.setTelephone(line[4]);
+
+
+                room.setNotes(line[5]);
+
+                room.setOffice(line[6]);
+
+                room.setRoomEmail(line[7]);
+                System.out.println("A new romm was submitted");
+            }else if (line.length==3){
+                System.out.println( "New user");
+            } else {
+                    System.out.println(line.length);
+                    System.out.println(line[0]);
+                    System.out.println(line[1]);
+
+                    throw new Exception();
+                }
+
+
+
+
+
+    } }catch(
+    Exception e)
+
+    {
+        System.out.println(" Exception: file is not a csv file or the layout does not fit.");
+    }
+}
+
+
+
+
 
 }
--- src/main/java/gpse/team52/web/DatamanagementController.java
+++ src/main/java/gpse/team52/web/DatamanagementController.java
@@ -1,24 +1,6 @@
 package gpse.team52.web;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.UUID;
-
-import javax.validation.Path;
-import javax.validation.Valid;
-
-import gpse.team52.contract.UserService;
-import gpse.team52.domain.User;
-import gpse.team52.exception.EmailExistsException;
-import gpse.team52.exception.InvalidConfirmationTokenException;
-import gpse.team52.exception.UsernameExistsException;
-import gpse.team52.form.UserRegistrationForm;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.config.RepositoryBeanDefinitionRegistrarSupport;
-import org.springframework.mail.MailException;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
+import gpse.team52.domain.DataImport;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
@@ -43,9 +25,12 @@ public class DatamanagementController {
 
     @PostMapping("/datamanagement")
     public ModelAndView submitData( @RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
-        System.out.println("flubb");
+
         //check if there is an uploaded file
        if (file != null && !file.isEmpty()) {
+           //check wich radio btn is enabled is needed
+           DataImport dataImport = new DataImport();
+           dataImport.csvImport(file);
 
            System.out.println("hallo");
            return new ModelAndView("redirect:/start");
